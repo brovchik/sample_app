68edc260efc6747ad0fb9f79d2d5b0315e726832
Yauhen Brouka <ybrouka@iba.by>
2015-09-29 11:04:37 +0300

    Added creating token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -105,4 +105,12 @@ describe User do
       it { should be_invalid }
     end
   end
+
+
+  # REMEMBER TOKEN
+  describe "remember token" do
+    before { @user.save }
+
+    its(:remember_token) { should_not be_blank }
+  end
 end
