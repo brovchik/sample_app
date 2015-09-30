64ce77e3544ee1d70e51c929940f1934d1973c7b
Yauhen Brouka <ybrouka@iba.by>
2015-09-30 14:59:30 +0300

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,8 +12,10 @@ describe User do
   it {should respond_to(:password_confirmation)}
   it { should respond_to(:remember_token) }
   it {should respond_to(:authenticate)}
+  it { should respond_to(:admin) }
 
   it { should be_valid }
+  it { should_not be_admin }
 
   #NAME
   describe "when name is not present" do
@@ -112,5 +114,16 @@ describe User do
     before { @user.save }
 
     its(:remember_token) { should_not be_blank }
+  end
+
+
+  # ADMIN
+  describe 'with admin attribute set to "true"' do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
   end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -85,8 +85,6 @@ describe 'Authentication' do
 
     let(:user)  { FactoryGirl.create(:user) }
 
-
-
     describe 'as wrong user' do
       let(:user) { FactoryGirl.create(:user) }
       let(:wrong_user) { FactoryGirl.create(:user, email: 'wrong@ex.com') }
@@ -101,6 +99,18 @@ describe 'Authentication' do
       describe 'submitting a PATCH request to the Users#update action' do
         before { patch user_path(wrong_user) }
 
+        specify { expect(response).to redirect_to(root_url) }
+      end
+    end
+
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin, no_capybara: true }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
         specify { expect(response).to redirect_to(root_url) }
       end
     end
