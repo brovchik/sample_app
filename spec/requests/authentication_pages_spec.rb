641b15a085f09d6d8669a77de98129ee40f2a337
Yauhen Brouka <ybrouka@iba.by>
2015-10-01 18:34:08 +0300

    Added Microposts controller

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -81,11 +81,23 @@ describe 'Authentication' do
         it { should_not have_link('Settings')}
         it { should_not have_link('Profile') }
       end
+
+      describe 'in the Microposts controller' do
+        describe 'submitting to the create action' do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe 'submitting to the destroy action' do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
     end
 
     let(:user)  { FactoryGirl.create(:user) }
 
-    describe 'as wrong user' do
+    describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
       let(:wrong_user) { FactoryGirl.create(:user, email: 'wrong@ex.com') }
       before { sign_in user, no_capybara: true }
