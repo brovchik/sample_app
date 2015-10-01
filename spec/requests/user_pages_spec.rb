8bed4667f72f6e734df465df4c8f43690a484ee6
Yauhen Brouka <ybrouka@iba.by>
2015-10-01 18:07:08 +0300

    Added view for microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "UserPages" do
-  subject {page}
+  subject { page }
 
   describe "SignUp page" do
     before { visit signup_path}
@@ -167,6 +167,22 @@ describe "UserPages" do
 
         it { should_not have_link('delete', href: user_path(admin)) }
       end
+    end
+  end
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
+    before { visit user_path(user) }
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
     end
   end
 
